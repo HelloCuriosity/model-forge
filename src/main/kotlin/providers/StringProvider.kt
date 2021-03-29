adee168bb56fcb72ac10cb5141babb78b9f7c332
Kyle Roe <hopeman15@users.noreply.github.com>
2021-03-29 11:20:49 +0200

    [Feature] StringProvider (#10)
    * WIP
    
    * string provider and test

---
+++ src/main/kotlin/providers/StringProvider.kt
@@ -0,0 +1,21 @@
+package providers
+
+import com.thedeanda.lorem.LoremIpsum
+
+class StringProvider(
+    private val wordCount: Int = DEFAULT_VALUE,
+    private val lorem: LoremIpsum = LoremIpsum.getInstance()
+) : Provider<String> {
+
+    companion object {
+        const val DEFAULT_VALUE = 1
+    }
+
+    override fun get(): String {
+        if (wordCount <= 0) {
+            println("$wordCount is not a valid input, reverting to default $DEFAULT_VALUE")
+            return lorem.getWords(wordCount)
+        }
+        return lorem.getWords(wordCount)
+    }
+}
---
+++ src/test/kotlin/providers/StringProviderTest.kt
@@ -0,0 +1,26 @@
+package providers
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class StringProviderTest {
+
+    @Test
+    fun testGet() {
+        for (i in 1..1000) {
+            val words = StringProvider(wordCount = i).get()
+            assertEquals(i, words.count())
+        }
+    }
+
+    @Test
+    fun testGet_ZeroOrLess() {
+        for (i in -1000..0) {
+            val words = StringProvider(wordCount = i).get()
+            assertEquals(1, words.count())
+        }
+    }
+
+    private fun String.count(): Int =
+        this.trim().splitToSequence(" ").count()
+}
