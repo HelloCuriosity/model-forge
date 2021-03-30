65068bca794a5cccbc20c10346a7ddcafb8c9290
Kyle <kyle.roe@thermondo.de>
2021-03-30 21:33:28 +0200

    kotlin random long

--- src/main/kotlin/providers/LongProvider.kt
+++ src/main/kotlin/providers/LongProvider.kt
@@ -1,18 +1,18 @@
 package providers
 
-import java.util.Random
+import kotlin.random.Random
 
 class LongProvider(
-    private val min: Int = DEFAULT_MIN,
-    private val max: Int = DEFAULT_MAX,
-    private val random: Random = Random()
+    private val min: Long = DEFAULT_MIN,
+    private val max: Long = DEFAULT_MAX,
+    private val random: Random = Random
 ) : Provider<Long> {
 
     companion object {
-        const val DEFAULT_MIN = 1
-        const val DEFAULT_MAX = 100000
+        const val DEFAULT_MIN: Long = 1
+        const val DEFAULT_MAX: Long = 100000
     }
 
     override fun get(): Long =
-        (min + random.nextInt(max - min)).toLong()
+        random.nextLong(min, max)
 }
