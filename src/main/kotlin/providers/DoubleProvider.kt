13d800bf76db543f3924bf2727678260401b5d17
Kyle <kyle.roe@thermondo.de>
2021-03-30 21:21:45 +0200

    double provider

---
+++ src/main/kotlin/providers/DoubleProvider.kt
@@ -0,0 +1,18 @@
+package providers
+
+import kotlin.random.Random
+
+class DoubleProvider(
+    private val min: Double = DEFAULT_MIN,
+    private val max: Double = DEFAULT_MAX,
+    private val random: Random = Random
+) : Provider<Double> {
+
+    companion object {
+        const val DEFAULT_MIN = 1.0
+        const val DEFAULT_MAX = 100000.0
+    }
+
+    override fun get(): Double =
+        random.nextDouble(min, max)
+}
