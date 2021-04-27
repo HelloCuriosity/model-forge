08ed92272bcd898a3443874e80960806e9ceb57e
Kyle <[email]>
2021-04-27 16:40:39 +0200

    [Release] Prepare Version 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "io.github.hellocuriosity"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 plugins {
     kotlin("jvm") version "1.4.32"
