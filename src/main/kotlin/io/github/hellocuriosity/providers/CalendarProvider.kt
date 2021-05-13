a372cd448f479655fc950a8560e6f43ded60806d
Kyle <kyle.roe@thermondo.de>
2021-05-13 16:50:35 +0200

    java doc

--- src/main/kotlin/io/github/hellocuriosity/providers/CalendarProvider.kt
+++ src/main/kotlin/io/github/hellocuriosity/providers/CalendarProvider.kt
@@ -2,6 +2,15 @@ package io.github.hellocuriosity.providers
 
 import java.util.Calendar
 
+/**
+ * Auto generates a Calendar instance between two dates
+ *
+ * @param from start date in millis
+ * @param until end date in millis
+ * @param dateProvider auto generate date for Calendar
+ *
+ * @return Instance of Calendar
+ */
 class CalendarProvider(
     private val from: Long = NINETEEN_EIGHTY_SIX,
     private val until: Long = TWENTY_TWENTY_ONE,
