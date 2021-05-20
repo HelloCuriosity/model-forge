0c0ec9c2a5d2463c3e062e7b94cb62325279223a
Kyle <[hopeman1986@gmail.com]>
2021-05-20 17:00:02 +0200

    docs

--- src/main/kotlin/io/github/hellocuriosity/providers/EnumProvider.kt
+++ src/main/kotlin/io/github/hellocuriosity/providers/EnumProvider.kt
@@ -3,6 +3,14 @@ package io.github.hellocuriosity.providers
 import io.github.hellocuriosity.ModelForgeException
 import kotlin.random.Random
 
+/**
+ * Auto generates an enum value
+ *
+ * @param <T> Type to instantiate
+ * @param random random generator
+ *
+ * @return enum value
+ */
 fun <T> Class<T>.getEnum(random: Random = Random): Any {
     if (enumConstants.isNotEmpty()) {
         val idx = random.nextInt(enumConstants.size)
