624a922eca422363ad37799149083449f8b1e28f
SuperSimple.io <code@supersimple.io>
2022-02-02 12:12:31 +0100

    More debug info on ModelForgeException

--- forge-core/src/main/kotlin/io/github/hellocuriosity/ModelForgeException.kt
+++ forge-core/src/main/kotlin/io/github/hellocuriosity/ModelForgeException.kt
@@ -2,4 +2,4 @@ package io.github.hellocuriosity
 
 import java.lang.RuntimeException
 
-class ModelForgeException(message: String) : RuntimeException(message)
+class ModelForgeException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
--- forge-core/src/main/kotlin/io/github/hellocuriosity/TryExt.kt
+++ forge-core/src/main/kotlin/io/github/hellocuriosity/TryExt.kt
@@ -1,9 +1,12 @@
 package io.github.hellocuriosity
 
-@Suppress("SwallowedException")
+@Suppress("SwallowedException", "TooGenericExceptionCaught", "PrintStackTrace")
 fun <T> Any.attempt(block: () -> T) = try {
     block()
 } catch (e: InstantiationError) {
     // Throw model forge exception instead
-    throw ModelForgeException("$this is not yet supported")
+    throw ModelForgeException("$this is not yet supported", e)
+} catch (e: Throwable) {
+    e.printStackTrace()
+    throw e
 }
