375c339612795e152caaf85c1617555cbd5a21f3
Kyle Roe <[hopeman15@users.noreply.github.com]>
2022-11-13 22:47:55 +0100

    logo.svg

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
         title: 'Model Forge',
         logo: {
           alt: 'Model Forge Logo',
-          src: 'img/logo.png', // svg maybe
+          src: 'img/logo.svg', // svg maybe
         },
         items: [
           {
